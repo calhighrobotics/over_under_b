aadb1c12e3b08b932e03539cea34158caf471779
Jay9971 <65046207+Jay9971@users.noreply.github.com>
2023-08-20 11:26:50 -0700

    added extra thingies

--- include/robot-config.h
+++ include/robot-config.h
@@ -8,6 +8,11 @@ extern motor RightFront;
 extern motor LeftBack;
 extern motor RightBack;
 extern controller Controller1;
+extern motor LeftMid;
+extern motor RightMid;
+extern motor Catapult;
+extern motor IntakeLeft;
+extern motor IntakeRight;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
